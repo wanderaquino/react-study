98ef1b56b4eae9585a376fd39706847f4e2195ae
Wander Aquino <wander.aquino@gmail.com>
2021-06-08 22:12:15 -0300

    Testando a renderização de componentes

---
+++ src/components/BomDia.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default (props) => {
+    return <h2>Bom dia, {props.nome}!</h2>
+} 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import First from "./components/First";
+import BomDia from "./components/BomDia"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//renderizar o primeiro elemento na página
+ReactDOM.render(<First/>, document.getElementById("root"));
+ReactDOM.render(<BomDia nome ="Charles"/>, document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
