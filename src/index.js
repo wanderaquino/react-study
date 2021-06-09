00a89e98cc9bc4e6f9389b57cb68831eb012acd4
Wander Aquino <wander.aquino@gmail.com>
2021-06-09 00:42:08 -0300

    Refletindo atualizações dos componentes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import First from "./components/First";
-import BomDia from "./components/BomDia";
-import {BoaTarde, BoaNoite} from "./components/Multiplos";
+// import First from "./components/First";
+// import BomDia from "./components/BomDia";
+// import {BoaTarde, BoaNoite} from "./components/Multiplos";
 import Saudacao from "./components/Saudacao"
 
 //renderizar o primeiro elemento na página
