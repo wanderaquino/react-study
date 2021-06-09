bd8d9c13c6344ef2b72b77151a4c9b40aff84c2b
Wander Aquino <wander.aquino@gmail.com>
2021-06-09 00:40:20 -0300

    Ajustando o componente para baseado em classe

--- src/components/Saudacao.jsx
+++ src/components/Saudacao.jsx
@@ -1,14 +1,34 @@
 import React, {Component} from "react";
 
 export default class Saudacao extends Component {
+    constructor(props) {
+        super(props);
+        
+        //Se não fizer esse bind, o this, não resolve para a instancia do componente.
+        this.setTipo = this.setTipo.bind(this);
+        this.setTipo = this.setNome.bind(this);
+    }
+    //Declarando o state
+    state = {
+        tipo: this.props.tipo,
+        nome: this.props.nome
+    }
+    setTipo(e) {
+        //Alterando o state para alterar o input
+        this.setState({tipo: e.target.value});
+    }
+    setNome(e) {
+        //Alterando o state para alterar o input
+        this.setState({tipo: e.target.value});
+    }
     render () {
-        const {tipo, nome} = this.props;
+        const {tipo, nome} = this.state;
         return (
             <div>
                 <h1>{tipo} {nome}</h1>
                 <br/>
-                <input type="text" placeholder="Tipo..." value={tipo}></input>
-                <input type="text" placeholder="Nome..." value={nome}></input>
+                <input type="text" placeholder="Tipo..." value={tipo} onChange={this.setTipo}></input>
+                <input type="text" placeholder="Nome..." value={nome} onChange={this.setNome}></input>
             </div>
         )
     }
