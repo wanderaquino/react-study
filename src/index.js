bb84432f9fb47fdd7bb23b44aeacd05c142868a0
Wander Aquino <wander.aquino@gmail.com>
2021-06-08 23:11:50 -0300

    Renderizando componente saudação

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import First from "./components/First";
-import BomDia from "./components/BomDia"
+import BomDia from "./components/BomDia";
+import {BoaTarde, BoaNoite} from "./components/Multiplos";
+import Saudacao from "./components/Saudacao"
 
 //renderizar o primeiro elemento na página
-ReactDOM.render(<First/>, document.getElementById("root"));
-ReactDOM.render(<BomDia nome ="Charles"/>, document.getElementById("root"));
+ReactDOM.render(
+    <div>
+        {/* <First/>
+        <BomDia nome="Charles"/>
+        <BoaTarde nome="Bia"/>
+        <BoaNoite nome="Joelma"/> */}
+        <Saudacao tipo="Bom dia!" nome="João"/>
+    </div>, 
+document.getElementById("root"));
 
