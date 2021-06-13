3e9ca2e8c4b01a7243f8d6f4bca28a3e8e84ab7d
Wander Aquino <wander.aquino@gmail.com>
2021-06-12 23:30:13 -0300

    Estudo sobre componentes pai e filho.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Pai from "./components/Pai";
+import Filho from "./components/Filho";
 // import First from "./components/First";
 // import BomDia from "./components/BomDia";
 // import {BoaTarde, BoaNoite} from "./components/Multiplos";
-import Saudacao from "./components/Saudacao"
+// import Saudacao from "./components/Saudacao"
 
 //renderizar o primeiro elemento na página
 ReactDOM.render(
@@ -12,7 +14,12 @@ ReactDOM.render(
         <BomDia nome="Charles"/>
         <BoaTarde nome="Bia"/>
         <BoaNoite nome="Joelma"/> */}
-        <Saudacao tipo="Bom dia!" nome="João"/>
+        {/* <Saudacao tipo="Bom dia!" nome="João"/> */}
+        <Pai nome="José" sobrenome="Alves">
+            <Filho nome="Carlota"></Filho>
+            {/* <Filho nome="Carla"></Filho>
+            <Filho nome="Carlina"></Filho> */}
+        </Pai>
     </div>, 
 document.getElementById("root"));
 
